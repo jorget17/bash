78005ebf1e2296d7f5d4485c855610c42d45f777
JorgeT17 <jorget17@gmail.com>
2021-05-27 16:58:04 +0200

    practicas

---
+++ practicas/0001_hola_mundo.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Ejercicio 0001: crear un script que imprima en pantalla el mensaje "Hola Mundo".
+
+echo Hola Mundo
---
+++ practicas/0002_hola_mundo_variable.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Ejercicio 0002: Mostrar en pantalla el texto "Hola Mundo" pero desde una variable.
+
+mensaje="Hola Mundo"
+echo $mensaje

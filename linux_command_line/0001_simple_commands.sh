d27d44f3c3999a3fea886205289d83af4d6935c4
JorgeT17 <jorget17@gmail.com>
2021-05-28 14:01:47 +0200

    linux command line

---
+++ linux_command_line/0001_simple_commands.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Display current time and date
+
+date
+
+# Display a calendar of the current month
+
+cal
+
+# Display the ammount of free space in disk drives
+
+df
+
+# Display the ammount of free memory
+
+free
+
+# Exist the current terminal session
+
+# exit
---
+++ linux_command_line/0002_navigation.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# The first thing to learn is how to navigate the file system
+
+# Commands to learn: pwd, cd, ls
+
+# The File System Tree --> it is a hierarchical directory structure.
